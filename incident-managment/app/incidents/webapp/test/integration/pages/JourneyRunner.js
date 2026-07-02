2ba65391e25750c42a876556bb303f94e673e167
Mohammed Irshad <mohammed.irshad@hornbilllabs.com>
2026-07-02 10:44:01 +0530

    fix:incidents. to incidents

--- incident-managment/app/incidents/webapp/test/integration/pages/JourneyRunner.js
+++ incident-managment/app/incidents/webapp/test/integration/pages/JourneyRunner.js
@@ -1,12 +1,12 @@
 sap.ui.define([
     "sap/fe/test/JourneyRunner",
-	"ns/incidents//test/integration/pages/IncidentsList",
-	"ns/incidents//test/integration/pages/IncidentsObjectPage"
+	"ns/incidents/test/integration/pages/IncidentsList",
+	"ns/incidents/test/integration/pages/IncidentsObjectPage"
 ], function (JourneyRunner, IncidentsList, IncidentsObjectPage) {
     'use strict';
 
     var runner = new JourneyRunner({
-        launchUrl: sap.ui.require.toUrl('ns/incidents/') + '/test/flp.html#app-preview',
+        launchUrl: sap.ui.require.toUrl('ns/incidents') + '/test/flp.html#app-preview',
         pages: {
 			onTheIncidentsList: IncidentsList,
 			onTheIncidentsObjectPage: IncidentsObjectPage
